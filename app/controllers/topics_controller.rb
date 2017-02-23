22df8ae34360ac8f60268ab0e98f0710a91bea77
osuke <osuke@TOKC02LQEXNFD57.local>
2017-02-23 21:50:25 +0900

    トピックのリファクタリング

--- app/controllers/topics_controller.rb
+++ app/controllers/topics_controller.rb
@@ -1,23 +1,45 @@
 class TopicsController < ApplicationController
+  before_action :set_topic, only: [:edit, :update, :destroy]
+
   def index
     @topics = Topic.all
   end
 
   def new
-    @topics = Topic.new
+    @topic = Topic.new
   end
 
   def edit
-      @topic = Topic.find(params[:id])
+  end
+
+  def update
+    if @topic.update(topics_params)
+      redirect_to topics_path, notice: "トピックを更新しました！"
+    else
+      render 'edit'
+    end
+  end
+
+  def destroy
+    @topic.destroy
+    redirect_to topics_path
   end
 
   def create
-    Topic.create(topics_params)
-    redirect_to topics_path
+    @topic=Topic.new(topics_params)
+    if @topic.save
+      redirect_to topics_path, notice: "トピックを作成しました！"
+    else
+      render 'new'
+    end
   end
 
   private
    def topics_params
      params.require(:topic).permit(:title, :content)
+   end
+
+   def set_topic
+     @topic=Topic.find(params[:id])
    end
 end
