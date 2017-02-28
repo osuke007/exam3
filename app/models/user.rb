c24a3a79c0c52f42e40f9cd7a47239a59da9ef4a
osuke <osuke@TOKC02LQEXNFD57.local>
2017-02-28 20:51:58 +0900

    SNSプロフ写真表示

--- app/models/user.rb
+++ app/models/user.rb
@@ -5,6 +5,7 @@ class User < ActiveRecord::Base
          :recoverable, :rememberable, :trackable,
          :validatable, :confirmable, :omniauthable
 
+         mount_uploader :avatar, AvatarUploader
 
   def self.find_for_facebook_oauth(auth, signed_in_resource=nil)
     user = User.find_by(email: auth.info.email)
@@ -44,5 +45,14 @@ class User < ActiveRecord::Base
 
   def self.create_unique_string
    SecureRandom.uuid
+  end
+
+  def update_with_password(params, *options)
+    if provider.blank?
+      super
+    else
+      params.delete :current_password
+      update_without_password(params, *options)
+    end
   end
 end
