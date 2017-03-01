3df777bb5b44771ccf3de5a1691bcb5a58fa07e5
osuke <osuke@TOKC02LQEXNFD57.local>
2017-03-01 19:44:08 +0900

    フォロー機能実装

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -1,4 +1,17 @@
 class CommentsController < ApplicationController
+
+
+  def edit
+  end
+
+  def update
+    if @comment.update(comments_params)
+      redirect_to commnets_path, notice: "トピックを更新しました！"
+    else
+      render 'edit'
+    end
+  end
+
   # コメントを保存、投稿するためのアクションです。
   def create
     # ログインユーザーに紐付けてインスタンス生成するためbuildメソッドを使用します。
@@ -18,6 +31,24 @@ class CommentsController < ApplicationController
         format.json { render json: @comment.errors, status: :unprocessable_entity }
       end
     end
+  end
+
+  def destroy
+      @comment = Comment.find(params[:id])
+      @comment.destroy
+
+      # respond_toは、クライアントからの要求に応じてレスポンスのフォーマットを変更します。
+      respond_to do |format|
+         if @comment.delete
+           format.html { redirect_to topic_path(@topic), notice: 'コメントを削除しました。' }
+           format.json { render :show, status: :created, location: @comment }
+           # JS形式でレスポンスを返します。
+           format.js { render :index }
+         else
+           format.html { render :new }
+           format.json { render json: @comment.errors, status: :unprocessable_entity }
+         end
+       end
   end
 
   private
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,10 @@
 Rails.application.routes.draw do
 
   root 'top#index'
+  
+  resources :relationships, only: [:create, :destroy]
+  resources :users, only: [:index]
+
   devise_for :users, controllers: {
      registrations: "users/registrations",
      omniauth_callbacks: "users/omniauth_callbacks"
